f40eb6c56e7b508309354edc3b2a90f5dcf244f7
srpnt3 <virushd93@gmail.com>
2023-04-19 18:24:21 +0200

    Memory Stuff
    Script to convert a single frame to (uncompressed) memory
    And tested in verilog

--- BadAppleRessources/convert.js
+++ BadAppleRessources/convert.js
@@ -1,23 +1,27 @@
 const fs = require('fs');
+const { buffer } = require('stream/consumers');
 const size = 128;
 
 fs.readFile('TestFrame.bmp', (err, data) => {
 
-	var buffer = [...data];
+	var memory = Buffer.alloc((128+16)*128);
 
-	// debug ascii art
-	let img = "";
-	let point = buffer[10]
-	for (let i = 0; i < size; i++) {
-		let line = "";
+	let point = data.readUInt8(10);
+	let o = 0;
+	for (let j = 0; j < size; j++) {
 		for (let i = 0; i < size; i++) {
-			let b = buffer[point], g = buffer[point+1], r = buffer[point+2];
-			if (!(r === g && g === b)) console.err("colors are not greyscale");
-			line += (r*g*b < 8290688) ? "  " : "00";
+			let c = data.readUInt8(point);
+			memory.write((c < 128) ? '0' : '1', o + i + j*128);
+			if ((i+1) % 8 == 0) {
+				o++;
+				memory.write(' ', o + i + j*128);
+			}
 			point += 3;
 		}
-		img = line + "\n" + img;
+		memory.write('\n', o + 127 + j*128);
 	}
 
-	console.log(img);
+	fs.writeFile('Memory.mem', memory, (err) => {
+		console.log('file saved i guess')
+	});
 })
